864ccabd5acc5366e62a0161ccc09a67b9dcf148
helmax-y <max_rad@ukr.net>
2022-10-06 20:02:21 +0300

    script replaced with iframe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 const App = () => (
-  <header className="header">
-    <p>Mietmecar</p>
-    <p className="header__desc">Rent a car service</p>
-  </header>
+  <div className="page">
+    <header className="header">
+      <p>Mietmecar</p>
+      <p className="header__desc">Rent a car service</p>
+    </header>
+    <main className="main">
+      <iframe
+        className="iframe"
+        title="Rent form"
+        src="https://rentsyst.com/settings/iframe-constructor/?token=C-UH3kgSYammaY_WlyJ68xWCOSwIkv05&id=2991"
+      />
+    </main>
+  </div>
 );
 
 export default App;
